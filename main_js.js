d87209153fda2b16442000af420f667b83620074
Cinthia Godoi <cinthiagodoi@hotmail.com>
2021-06-21 20:20:11 +0100

    change functions to es6

--- main_js.js
+++ main_js.js
@@ -1,44 +1,43 @@
-document.body.onload = createElements(16);
 
-function createElements(gridNumber){
-  let containerWrapper = document.getElementById('container-wrapper')
-  let gridArea = gridNumber * gridNumber;
-  let container = document.createElement('div')
-  container.setAttribute('class', 'container');
-  container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`
-  container.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`
-  containerWrapper.appendChild(container);
-   
-  for(let i = 0; i < gridArea; i++){
-    let item = document.createElement('div')
-    container.appendChild(item); 
-    item.addEventListener("mouseover", generateColor);
-  } 
-}
-
-function generateColor(){
-  let opacity = Number(this.style.backgroundColor.slice(-4,-1));
-  if(opacity < 0.9){
-    this.style.backgroundColor = `rgba(0, 0, 0, ${opacity + 0.1}`;
-  }
-}
-
-function showGrid() {
+const showGrid = () => {
   let wrapper = document.getElementById('container-wrapper')
   while(wrapper.firstChild) {
     wrapper.removeChild(wrapper.firstChild);
   } 
 }
 
-function resetPage() {
-  let gridSize = document.getElementById('grid-size')
-  let gridNumber = gridSize.value;
-  showGrid();
-  createElements(gridNumber)
+const createElements = (gridNumber) => {
+    let containerWrapper = document.getElementById('container-wrapper')
+    let gridArea = gridNumber * gridNumber;
+    let container = document.createElement('div')
+    container.setAttribute('class', 'container');
+    container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`
+    containerWrapper.appendChild(container);
+      
+    for(let i = 0; i < gridArea; i++){
+      let item = document.createElement('div')
+      container.appendChild(item); 
+      item.addEventListener("mouseover", generateColor);
+    } 
+  }
 
-}
+  const generateColor = (event) => {
+    let square = event.target
+    let opacity = Number(square.style.backgroundColor.slice(-4,-1));
+    if(opacity < 0.9){
+      square.style.backgroundColor = `rgba(0, 0, 0, ${opacity + 0.1}`;
+    }
+  }
+
+  const resetPage = () => {
+    let gridSize = document.getElementById('grid-size')
+    let gridNumber = gridSize.value;
+    showGrid();
+    createElements(gridNumber)
+  }
+
   let button = document.getElementById('resize')
-    button.addEventListener("click", resetPage)
-
-
-
+  button.addEventListener("click", resetPage)
+ 
+  document.body.onload = createElements(16);
